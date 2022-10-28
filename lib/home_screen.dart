920594eb2631028571ee0e1b8ed571bda11855ab
Phatsakorn Chosungnoen <phatsakorn.newz@gmail.com>
2022-10-28 10:43:04 +0700

    [Edits] It's not work for my projects

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,15 +1,210 @@
+// import 'package:flutter/material.dart';
+// import 'package:sidebarx/sidebarx.dart';
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   var onClick = false;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Row(
+//         children: [
+//           SidebarX(
+//             controller: SidebarXController(selectedIndex: 0),
+//             items: const [
+//               SidebarXItem(
+//                 icon: Icons.home,
+//                 label: 'Home'),
+//               SidebarXItem(
+//                 icon: Icons.search,
+//                 label: 'Search'),
+//             ],
+//           ),
+//           // Your app screen body
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+import 'dart:math' as math show pi;
+
+import 'package:collapsible_sidebar/collapsible_sidebar.dart';
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget {
+void main() => runApp(const MyHomePage());
+
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sidebar ui',
+      home: Scaffold(
+        body: const SidebarPage(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+              content: Text('Yay! Button Pressed!'),
+            ));
+          },
+          backgroundColor: Colors.green,
+          child: const Icon(Icons.navigation),
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class SidebarPage extends StatefulWidget {
+  const SidebarPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SidebarPageState createState() => _SidebarPageState();
+}
+
+class _SidebarPageState extends State<SidebarPage> {
+  late List<CollapsibleItem> _items;
+  late String _headline;
+  final AssetImage _avatarImg = const AssetImage('assets/man.png');
+
+  @override
+  void initState() {
+    super.initState();
+    _items = _generateItems;
+    _headline = _items.firstWhere((item) => item.isSelected).text;
+  }
+
+  List<CollapsibleItem> get _generateItems {
+    return [
+      CollapsibleItem(
+        text: 'Dashboard',
+        icon: Icons.assessment,
+        onPressed: () => setState(() => _headline = 'DashBoard'),
+        isSelected: true,
+      ),
+      CollapsibleItem(
+        text: 'Ice-Cream',
+        icon: Icons.icecream,
+        onPressed: () => setState(() => _headline = 'Errors'),
+      ),
+      CollapsibleItem(
+        text: 'Search',
+        icon: Icons.search,
+        onPressed: () => setState(() => _headline = 'Search'),
+      ),
+      CollapsibleItem(
+        text: 'Notifications',
+        icon: Icons.notifications,
+        onPressed: () => setState(() => _headline = 'Notifications'),
+      ),
+      CollapsibleItem(
+        text: 'Settings',
+        icon: Icons.settings,
+        onPressed: () => setState(() => _headline = 'Settings'),
+      ),
+      CollapsibleItem(
+        text: 'Home',
+        icon: Icons.home,
+        onPressed: () => setState(() => _headline = 'Home'),
+      ),
+      CollapsibleItem(
+        text: 'Alarm',
+        icon: Icons.access_alarm,
+        onPressed: () => setState(() => _headline = 'Alarm'),
+      ),
+      CollapsibleItem(
+        text: 'Eco',
+        icon: Icons.eco,
+        onPressed: () => setState(() => _headline = 'Eco'),
+      ),
+      CollapsibleItem(
+        text: 'Event',
+        icon: Icons.event,
+        onPressed: () => setState(() => _headline = 'Event'),
+      ),
+      CollapsibleItem(
+        text: 'Email',
+        icon: Icons.email,
+        onPressed: () => setState(() => _headline = 'Email'),
+      ),
+      CollapsibleItem(
+        text: 'Face',
+        icon: Icons.face,
+        onPressed: () => setState(() => _headline = 'Face'),
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: CollapsibleSidebar(
+        // collapseOnBodyTap: false,
+        isCollapsed: MediaQuery.of(context).size.width <= 800,
+        items: _items,
+        // avatarImg: _avatarImg,
+        // title: 'John Smith',
+        onTitleTap: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Yay! Flutter Collapsible Sidebar!')));
+        },
+        body: _body(size, context),
+        // backgroundColor: Colors.black,
+        selectedTextColor: Colors.limeAccent,
+        // textStyle: const TextStyle(fontSize: 15, fontStyle: FontStyle.italic),
+        titleStyle: const TextStyle(
+            fontSize: 20,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold),
+        toggleTitleStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        // sidebarBoxShadow: const [
+        //   BoxShadow(
+        //     color: Colors.indigo,
+        //     blurRadius: 20,
+        //     spreadRadius: 0.01,
+        //     offset: Offset(3, 3),
+        //   ),
+        //   BoxShadow(
+        //     color: Colors.green,
+        //     blurRadius: 50,
+        //     spreadRadius: 0.01,
+        //     offset: Offset(3, 3),
+        //   ),
+        // ],
+      ),
+    );
+  }
+
+  Widget _body(Size size, BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      color: Colors.grey,
+      child: Center(
+        child: Transform.rotate(
+          angle: math.pi / 2,
+          child: Transform.translate(
+            offset: Offset(-size.height * 0.3, -size.width * 0.23),
+            child: Text(
+              _headline,
+              style: Theme.of(context).textTheme.headline1,
+              overflow: TextOverflow.visible,
+              softWrap: false,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
